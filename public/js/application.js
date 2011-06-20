8697ec44926e155452f94768ec7316b389d10422
Mo Lawson <moklawson@gmail.com>
2011-06-20 10:35:56 -0500

    better (more apple-esque) loading icon

--- public/js/application.js
+++ public/js/application.js
@@ -25,7 +25,7 @@ $.fn.extend({
   }, // end quickClick
   
   loadingScreen: function() {
-    this.html('<div class="spinner"><div class="bar1"></div><div class="bar2"></div><div class="bar3"></div><div class="bar4"></div><div class="bar5"></div><div class="bar6"></div><div class="bar7"></div><div class="bar8"></div>');
+    this.html('<div class="loading"><div class="spinner"><div class="bar1"></div><div class="bar2"></div><div class="bar3"></div><div class="bar4"></div><div class="bar5"></div><div class="bar6"></div><div class="bar7"></div><div class="bar8"></div><div class="bar9"></div><div class="bar10"></div><div class="bar11"></div><div class="bar12"></div></div>Loading...</div>');
   } // end loadingScreen
 });
 
