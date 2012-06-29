7825627489d02123cb1e84bc40a1628920e24afe
Mo Lawson <moklawson@gmail.com>
2012-06-29 11:40:13 -0500

    use Zepto instead of jQuery for speed/size purposes. better commenting of js.

--- public/js/application.js
+++ public/js/application.js
@@ -1,28 +1,107 @@
-jQuery.ajaxSetup({ 
-	"beforeSend": function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} 
-});
-
-$.fn.extend({
+$.extend($.fn, {
   // use touch events to simulate clicks (much more responsive)
   quickClick: function(callback) {
     this.live('touchstart', function(e) {
   	  moved = false;
+
+      // letting us make things looks pretty
   	  $(this).addClass('pressed');
+
   		$(this).bind('touchmove', function() {
+        // differentiating moves (e.g. swipes) from taps
   		  moved = true;
     	  $(this).removeClass('pressed');
   		});
+
   		$(this).bind('touchend', function(e) {
   		  e.preventDefault();
+
+        // cleaning up after ourselves
   		  $(this).unbind('touchend');
   		  $(this).unbind('touchmove');
     	  $(this).removeClass('pressed');
+
+        // only perform the callback if the touch was a tap
+        // rather than a swipe or other movement
   		  if (!moved) {
   		    callback($(this));
   		  }
   		});
+
   	});
   }, // end quickClick
+
+  slideDown: function(duration, easing, callback) {
+    duration = duration || 400;
+    easing = easing || 'ease';
+
+    // element will be hidden but will allow us to get its intended height
+    this.css('visibility', 'hidden');
+    this.show();
+
+    // get the height of the object if it weren't hidden
+    var intendedHeight = this.height() + 'px';
+
+    // storing the element for post animation actions
+    var that = this;
+
+    // now that we have the height, we can put things back for our animation
+    this.css('height', '0px');
+    this.css('visibility', 'visible');
+
+    this.animate({ height: intendedHeight }, duration, easing, function() {
+      // leave no trace
+      that.css('height', 'auto');
+
+      if(callback) {
+        callback($(this));
+      }
+    });
+  }, // end slideDown
+
+  slideUp: function(duration, easing, callback) {
+    duration = duration || 400;
+    easing = easing || 'ease';
+
+    // we need to get its current height so we can put things back once we're done,
+    var actualHeight = this.height() + 'px';
+
+    // storing the element for post animation actions
+    var that = this;
+
+    this.css('height', actualHeight);
+
+    this.animate({ height: '0' }, duration, easing, function() {
+      // making sure it's gone
+      that.hide();
+
+      // element will be hidden but will allow us to get its intended height
+      that.css('visibility', 'hidden');
+      that.css('height', 'auto');
+      that.show();
+
+      // get the height of the object if it weren't hidden, and use that 
+      // to set the element's height explicitly (leave no trace)
+      var intendedHeight = that.height() + 'px';
+      that.css('height', intendedHeight);
+
+      // returning things to a normalized state
+      that.css('visibility', 'visible');
+      that.hide();
+
+      if(callback) {
+        callback($(this));
+      }
+    });
+  }, // end slideUp
+
+  slideToggle: function(duration, easing, callback) {
+    if(this.css('display') == 'none') {
+      this.slideDown(duration, easing, callback)
+    } else {
+      this.slideUp(duration, easing, callback)
+    }
+  }, // end slideToggle
   
   loadingScreen: function() {
     this.html('<div class="loading"><div class="spinner"><div class="bar1"></div><div class="bar2"></div><div class="bar3"></div><div class="bar4"></div><div class="bar5"></div><div class="bar6"></div><div class="bar7"></div><div class="bar8"></div><div class="bar9"></div><div class="bar10"></div><div class="bar11"></div><div class="bar12"></div></div>Loading...</div>');
@@ -42,7 +121,7 @@ $(document).ready(function() {
     tab = target.attr('id');
     $('nav ul li').removeClass('active');
     $('nav ul li#' + tab).addClass('active');
-    $.get('/' + tab, null, function (data) {
+    $.get('/' + tab, function (data) {
       clearTimeout(loading);
       $("#main").html(data);
     });
@@ -50,13 +129,16 @@ $(document).ready(function() {
   
   // open/close actions drawer for individuals list
   $('ul#guests li .content').quickClick(function(target) {
-    target.siblings('.drawer').slideToggle('fast');
-    target.siblings('.drawer').children('.details_box').slideUp('fast');
+    var detailsBox = target.siblings('.drawer').children('.details_box');
+    if( detailsBox.css('display') != 'none' ) {
+      detailsBox.slideUp(100);
+    }
+    target.siblings('.drawer').slideToggle(200);
   });
 
 	// show/hide guest details
 	$('ul#guests li .drawer .button.details').quickClick(function(target) {
-			target.siblings('.details_box').slideToggle('fast');
+			target.siblings('.details_box').slideToggle(100);
 	});
 
 	// check guests in and out
@@ -85,7 +167,7 @@ $(document).ready(function() {
 	// group name in guest details links to group list
 	$('ul#guests li .drawer .details_box .guest_group').quickClick(function(target) {
 		loading = delayedLoadingScreen();
-		$.get('/groups/' + target.attr('id'), null, function(data) {
+		$.get('/groups/' + target.attr('id'), function(data) {
 			clearTimeout(loading);
 			$('nav ul li').removeClass('active');
 	    $('nav ul li#groups').addClass('active');
@@ -96,7 +178,7 @@ $(document).ready(function() {
   // go to list of group attendess when clicking on a group name
   $('ul#groups li .content').quickClick(function(target) {
     loading = delayedLoadingScreen();
-    $.get('/groups/' + target.parent('li').attr('id'), null, function(data) {
+    $.get('/groups/' + target.parent('li').attr('id'), function(data) {
       clearTimeout(loading);
       $("#main").html(data);
     });
@@ -105,7 +187,7 @@ $(document).ready(function() {
   // go to a list of guests of a certain type when selected form the "more" tab
   $('table#stats tr td.guest_type').quickClick(function(target) {
     loading = delayedLoadingScreen();
-    $.get('/individuals/type/' + target.parent('tr').attr('id'), null, function(data) {
+    $.get('/individuals/type/' + target.parent('tr').attr('id'), function(data) {
       clearTimeout(loading);
       $('#main').html(data);
     });
@@ -115,7 +197,7 @@ $(document).ready(function() {
   $('table.alpha td').quickClick(function(target) {
     loading = delayedLoadingScreen();
     active = $('nav ul li.active').attr('id');
-    $.get('/' + active + '/alpha/' + target.html(), null, function(data) {
+    $.get('/' + active + '/alpha/' + target.html(), function(data) {
       clearTimeout(loading);
       $("#main").html(data);
     });
@@ -132,7 +214,7 @@ $(document).ready(function() {
 	$('ul.scope li .reset').quickClick(function() {
 	  loading = delayedLoadingScreen();
 	  active = $('nav ul li.active').attr('id');
-	  $.get('/' + active, null, function (data) {
+	  $.get('/' + active, function (data) {
       clearTimeout(loading);
       $("#main").html(data);
     });
@@ -143,12 +225,12 @@ $(document).ready(function() {
     if (!$('#search_results').length) {
       $('table.alpha').after('<div id="search_results"></div>');      
     }
-    $('table.alpha').fadeOut('fast');
+    $('table.alpha').hide();
   });
   $('#search input').live('blur', function() {
     if ($(this).val() === '') {
       $('#search_results').remove();
-      $('table.alpha').fadeIn('fast');
+      $('table.alpha').show();
     }
   });
 
@@ -162,7 +244,7 @@ $(document).ready(function() {
 		} else {
 			$(this).siblings('.reset').hide();
 		}
-		$.get('/' + active + '/search?query=' + query, null, function (data) {
+		$.get('/' + active + '/search?query=' + query, function (data) {
 		  clearTimeout(loading);
       $("#search_results").html(data);
     });
