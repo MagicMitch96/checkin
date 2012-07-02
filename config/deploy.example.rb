99636dc855e8b4acf64a15db16607bbede3fd045
Mo Lawson <moklawson@gmail.com>
2012-07-02 16:36:34 -0500

    update example cap deploy file.

--- config/deploy.example.rb
+++ config/deploy.example.rb
@@ -29,23 +29,26 @@ namespace :deploy do
   end
   
   task :create_config_files do
+    default_admin_login = <<-EOF
+    USERNAME: #{user}
+    PASSWORD: #{password}
+    EOF
+    
+    admin_login = ERB.new(default_admin_login)
+
     run "mkdir -p #{shared_path}/config" 
-    run "cp #{current_path}/config/admin_login.example.yml #{shared_path}/config/admin_login.yml" 
-    run "cp #{current_path}/config/nginx.example.yml #{shared_path}/config/nginx.yml" 
-    run "cp #{current_path}/config/unicorn_init.example.yml #{shared_path}/config/unicorn_init.yml" 
+    put admin_login.result, "#{shared_path}/config/admin_login.yml"
   end
-  after "deploy:setup", "deploy:create_config_files"
 
   task :symlink_config_files do
     run "ln -nfs #{shared_path}/config/admin_login.yml #{release_path}/config/admin_login.yml" 
   end
-  after "deploy:update_code", "deploy:symlink_config_files" 
 
   task :setup_config, roles: :app do
-    sudo "ln -nfs #{shared_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{shared_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
   end
-  after "deploy:create_config_files", "deploy:setup_config"
+  after "deploy:setup", "deploy:setup_config"
   
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
@@ -58,19 +61,44 @@ namespace :deploy do
   before "deploy", "deploy:check_revision"
 end
 
+before "deploy:setup", "deploy:create_config_files"
+after "deploy:update_code", "deploy:symlink_config_files" 
 
 namespace :db do
   desc "Create mongoid yaml in shared path" 
   task :default do
+    default_template = <<-EOF
+    development:
+      sessions:
+        default:
+          database: checkin_development
+          hosts:
+            - localhost:27017
+    production:
+      sessions:
+        default:
+          database: checkin_production
+          hosts:
+            - localhost:27017
+    test:
+      sessions:
+        default:
+          database: checkin_test
+          hosts:
+            - localhost:27017
+    EOF
+    
+    db_config = ERB.new(default_template)
+
     run "mkdir -p #{shared_path}/config" 
-    run "cp #{current_path}/config/mongoid.example.yml #{shared_path}/config/mongoid.yml" 
+    put db_config.result, "#{shared_path}/config/mongoid.yml" 
   end
-  after "deploy:setup", :db
 
   desc "Make symlink for mongoid yaml" 
   task :symlink do
     run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml" 
   end
-  after "deploy:update_code", "db:symlink" 
 end
 
+before "deploy:setup", :db
+after "deploy:update_code", "db:symlink" 
